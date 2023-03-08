0da4a337b04677ee3541d0ecf138607fd873ae34
Chrysite <3022909548@qq.com>
2023-03-08 14:02:48 +0800

    增加计时功能，时间数据存入test.db中的user表中；实验窗口中的返回按钮可以返回主窗口，关闭按钮改为关闭整个程序

--- tools/SqlTools.py
+++ tools/SqlTools.py
@@ -4,13 +4,11 @@ connect = sqlite3.connect("test.db")
 
 cur = connect.cursor()
 
-
 def get_question_by_chapter(chapter):
     sql_str = "SELECT * from main.test WHERE chapter = \'" + chapter + "\'"
     cur.execute(sql_str)
     data = cur.fetchall()
     return data
-
 
 def get_correct_answer_by_chapter(chapter):
     data = get_question_by_chapter(chapter)
@@ -18,5 +16,19 @@ def get_correct_answer_by_chapter(chapter):
     for question in data:
         correct_answer.append(question[7])
     return correct_answer
+
+def get_user(id):
+    sql_str = "SELECT * from user WHERE id =\'" + id + "\'"
+    cur.execute(sql_str)
+    data = cur.fetchall()
+    return data
+
+def update_user(sec_list):
+    # sql_str = "UPDATE user set time_1 =" + sec_list[0] + ",time_2 =" + sec_list[1] + ",time_3 =" + sec_list[2] +",time_4 =" + sec_list[3]+",time_5 =" + sec_list[4]+",time_6 =" + sec_list[5]+",time_7 =" + sec_list[6]+",time_8 =" + sec_list[7]+",time_9 =" + sec_list[8]+",total_time =" + sec_list[9]+"WHERE id=admin"
+    sql_str = "UPDATE user SET time_1 =?,time_2 =?,time_3 =?,time_4 =?,time_5 =?,time_6 =?,time_7 =?,time_8 =?,time_9 =?,total_time =? WHERE id=\'admin\'"
+    cur.execute(sql_str,sec_list)
+    connect.commit()
+
+
 
 
