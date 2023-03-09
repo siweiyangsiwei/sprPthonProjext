9dbd6524f39da2235ad678f429afa0e462349b07
Xavier <xavier@qq.com>
2023-03-09 22:58:04 +0800

    打开登录界面

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from controllers.MainWindow import MainWindow
 if __name__ == '__main__':
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
-    # login_window = LoginWindow()
-    # login_window.show()
-    main_window = MainWindow()
-    main_window.show()
+    login_window = LoginWindow()
+    login_window.show()
+    # main_window = MainWindow()
+    # main_window.show()
     sys.exit(app.exec_())
